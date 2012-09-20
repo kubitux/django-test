61a7b952841325b3869984288b624a424f92e8a4
Sebastien <sebastien.c@swelen.net>
2012-09-20 16:06:06 +0200

    Using generic views to avoid redundancy --> DOESN'T WORK YET

--- polls/views.py
+++ polls/views.py
@@ -4,25 +4,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 
-def index(request):
-	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	t = loader.get_template('polls/index.html')
-	c = {'latest_poll_list' : latest_poll_list}
-
-	return render_to_response('polls/index.html', c)
-
-
-def detail(request, poll_id):
-	p = get_object_or_404(Poll, pk=poll_id)
-	return render_to_response('polls/detail.html', {'poll':p}, 
-				  context_instance=RequestContext(request))
-
-
-def results(request, poll_id):
-	p = get_object_or_404(Poll, pk=poll_id)
-	return render_to_response('polls/results.html',{'poll':p})
-
-
 
 def vote(request, poll_id):
 
